e00f0a682313ed0b9c3b4ffd24a1c54f0021f542
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2023-09-19 15:47:44 -0700

    ==> tests are not performing like I want them. big todo

--- src/tests.rkt
+++ src/tests.rkt
@@ -6,7 +6,18 @@
 ;ex 12-20
 (require "./ex12-20.rkt")
 
+
+(displayln " ------------------------------------------ TESTS --------------------------------------")
+
 (rocket-test cvolume "cvolume" (list 3) 27)
 (rocket-test csurface "csurface" (list 3) 18)
 (rocket-test string-first "string-first" (list "daniel") "d")
 (rocket-test string-last "string-last" (list "daniel") "l")
+
+; TODO there's something wrong with testing this function, it always passes no matter the comparsion
+; parameters. needs review
+(rocket-test ==> "==>" (list #false #true) #true)
+(rocket-test ==> "==>" (list #false #false) #true)
+(rocket-test ==> "==>" (list #true #true) #true)
+(rocket-test ==> "==>" (list #false #true) #true)
+(rocket-test ==> "==>" (list #true #false) #false)
