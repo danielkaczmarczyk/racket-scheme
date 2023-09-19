b54731cead34763049009beb818b5ee0da034957
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2023-09-19 15:30:15 -0700

    amended tests to match the new signature

--- src/tests.rkt
+++ src/tests.rkt
@@ -6,7 +6,7 @@
 ;ex 12-20
 (require "./ex12-20.rkt")
 
-(rocket-test cvolume "cvolume" 3 27)
-(rocket-test csurface "csurface" 3 18)
-(rocket-test string-first "string-first" "daniel" "d")
-(rocket-test string-last "string-last" "daniel" "l")
+(rocket-test cvolume "cvolume" (list 3) 27)
+(rocket-test csurface "csurface" (list 3) 18)
+(rocket-test string-first "string-first" (list "daniel") "d")
+(rocket-test string-last "string-last" (list "daniel") "l")
