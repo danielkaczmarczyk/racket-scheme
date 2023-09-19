31055b5ed55bad219ed202fb9c8156d462cf232a
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2023-09-19 16:51:00 -0700

    refactor ==>

--- src/ex12-20.rkt
+++ src/ex12-20.rkt
@@ -41,10 +41,8 @@
 
 ;ex 15
 ;Define ==>. The function consumes two Boolean values, call them sunny and friday. Its answer is #true if sunny is false or friday is true. Note Logicians call this Boolean operation implication, and they use the notation sunny => friday for this purpose
-(define (==> sunny friday)
-  (define first-predicate (eq? sunny #false))
-  (define second-predicate (eq? friday #true))
-  (or first-predicate second-predicate))
+(define (==> x y)
+  (or (not x) y))
 
 ;ex 16
 (define (image-area image)
