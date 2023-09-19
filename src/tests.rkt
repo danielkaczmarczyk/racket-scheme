744398e10c2b81991449b760db3935030239f14b
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2023-09-19 02:53:47 -0700

    add new tests

--- src/tests.rkt
+++ src/tests.rkt
@@ -3,9 +3,10 @@
 ;import testing framework
 (require "./rocket.rkt")
 
-;ex 12
+;ex 12-20
 (require "./ex12-20.rkt")
 
 (rocket-test cvolume "cvolume" 3 27)
 (rocket-test csurface "csurface" 3 18)
 (rocket-test csurface "csurface" 3 19)
+(rocket-test string-first "string-first" "daniel" "d")
