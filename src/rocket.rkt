68f6d8a366537c89e0889ad6ecc53f24ef8497ac
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2023-09-19 02:34:04 -0700

    added some helper methods

--- src/rocket.rkt
+++ src/rocket.rkt
@@ -3,15 +3,34 @@
 
 (provide rocket-test)
 
-(define (rocket-test fn input expected)
+(define (stringify atom)
+  (cond 
+    [(number? atom) (number->string atom)]
+    [else atom]))
+
+(define (generate-test-message 
+          prefix
+          fn_name
+          input
+          output
+          expected)
+
+  (string-append 
+    (string-append
+      prefix ": " fn_name " called with " (stringify input) " produced " (stringify output))
+    (if (eq? prefix "FAIL")
+      (" expected: " (stringify expected))
+      "")))
+
+(define (rocket-test fn fn_name input expected)
   (define result (fn input))
 
   (if
     (eq?
       result
       expected)
-    (displayln "PASS")
-    (displayln "FAIL")))
+    (displayln (generate-test-message "PASS" fn_name input result expected))
+    (displayln (generate-test-message "FAIL" fn_name input result expected))))
 
 
 ;(define (rocket-suite))
