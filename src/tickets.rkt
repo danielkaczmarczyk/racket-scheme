edd3a4652ea5cbaa3125f3fcf6f180d1f3a3637c
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2023-09-19 22:57:46 -0700

    ex27

--- src/tickets.rkt
+++ src/tickets.rkt
@@ -1,15 +1,23 @@
 #lang racket
 
-(provide attendees)
+(provide profit)
+
+(define BASE_ATTENDANCE 120)
+(define TICKET_PRICE_CHANGE_STEP 0.1) ; in dollars
+(define TICKET_PRICE_CHANGE_ATTENDANCE 15) ; n of people that TICKET_PRICE_CHANGE_STEP affects
+(define BASE_TICKET_PRICE 5.0)
+(define FIXED_COST 180)
+(define COST_VARIABLE_PER_PERSON 0.04)
+
 
 (define (attendees ticket-price)
-  (- 120 (* (- ticket-price 5.0) (/ 15 0.1))))
+  (- BASE_ATTENDANCE (* (- ticket-price BASE_TICKET_PRICE) (/ TICKET_PRICE_CHANGE_ATTENDANCE TICKET_PRICE_CHANGE_STEP))))
 
 (define (revenue ticket-price)
   (* ticket-price (attendees ticket-price)))
 
 (define (cost ticket-price)
-  (+ 180 (* 0.04 (attendees ticket-price))))
+  (+ FIXED_COST (* COST_VARIABLE_PER_PERSON (attendees ticket-price))))
 
 (define (profit ticket-price)
   (- (revenue ticket-price)
