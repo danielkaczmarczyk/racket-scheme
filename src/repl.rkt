6e3ff0ed7cc89a9bed80c05e762e9b53353aba83
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2023-09-21 20:39:04 -0700

    move repl

--- src/repl.rkt
+++ src/repl.rkt
@@ -1,32 +1,3 @@
-#lang racket
-
-(require 2htdp/image)
-(require 2htdp/universe)
-
-(define (number->square s)
-  (square s "solid" "red"))
-
-(define small (number->square 5))
-(define medium (number->square 10))
-(define large (number->square 15))
-
-(define images (list small medium large))
-(define filenames (list "small" "medium" "large"))
-(define filenames-full (map (lambda (filename) (string-append filename ".png")) filenames))
-
-(define (save-images)
-  (for-each save-image images filenames-full))
-
-(define (bang)
-  (big-bang 100
-    [to-draw number->square]
-    [on-tick sub1]
-    [stop-when zero?]))
-            
-
-(define (run)
-  (bang))
-
 (define (handle-input input-string)
   (cond
     [(string=? input-string "bang") (bang)]
@@ -47,6 +18,4 @@
       (printf "")
       (handle-input a))
     (loop)))
-
-(bang)
 
