8318f1e17a6d30ac8e65df469c2874612bc28041
Daniel Kaczmarczyk <daniel.kaczmarczyk@hey.com>
2023-09-21 20:40:00 -0700

    remove repl code from bang

--- src/big-bang/bang.rkt
+++ src/big-bang/bang.rkt
@@ -27,26 +27,6 @@
 (define (run)
   (bang))
 
-(define (handle-input input-string)
-  (cond
-    [(string=? input-string "bang") (bang)]
-    [(string=? input-string "exit") (displayln "Exiting...") (exit)]
-    [else print-debug-input-info input-string]))
-
-(define (print-debug-input-info input-string)
-  (printf "input: ~a, length: ~a, last character: ~a\n"
-        input-string 
-        (string-length  input-string)
-        (char->integer (string-ref  input-string (- (string-length input-string) 1)))))
-
-(define (repl)
-  (let loop()
-    (printf "$: ")
-    (define a (read-line (current-input-port) 'any))
-    (if (= (string-length a) 0)
-      (printf "")
-      (handle-input a))
-    (loop)))
 
 (bang)
 
